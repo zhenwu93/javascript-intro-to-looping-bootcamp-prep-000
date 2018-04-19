6af8d1b7b970e00059cd284d1e987613dae9cfa5
Zhen Wei Wu <zhenwu93@gmail.com>
2018-04-19 22:28:46 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,7 +21,7 @@ function doWhileLoop(array) {
   return Math.random() >= 0.5
 }
   do {
-    array.slice(-1);
+    array.slice(--1);
  } while (array.length > 0 && maybeTrue()); {
    return array;
  }
